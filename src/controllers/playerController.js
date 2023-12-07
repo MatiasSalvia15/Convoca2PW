514136804b245f6797ccea39646f1e346d339037
Matias <matielcruck1414@gmail.com>
2023-12-07 09:09:02 -0300

    playerController con un get para obtener todos los jugadores

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,3 +1,16 @@
-const helloWorld = (req, res)=> (res.send('Hello world!'))
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-export {helloWorld}
+
+
+
+const obtenerTodos = async (req, res) => {
+  try {
+    const jugadores = await prisma.player.findMany();
+    res.json({ jugadores });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al obtener los jugadores' });
+  }
+};
+export {obtenerTodos}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
-import {helloWorld} from './controllers/playerController.js'
+import { obtenerTodos } from "./controllers/playerController.js";
 const router = Router();
 
-router.get('/', helloWorld)
+router.get('/jugadores', obtenerTodos)
 
 export {router}
